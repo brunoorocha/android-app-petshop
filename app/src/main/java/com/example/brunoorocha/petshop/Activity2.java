c1ef3342d05fdbd55cde23d0af538e1d1d826de1
CajuWEB <36238396+CajuWEB@users.noreply.github.com>
2018-11-18 12:21:41 -0200

    Implementação Comentarios usando activty e contentProvider

---
+++ app/src/main/java/com/example/brunoorocha/petshop/Activity2.java
@@ -0,0 +1,52 @@
+package com.example.brunoorocha.petshop;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class Activity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_2);
+    }
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        InputMethodManager imm = (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
+        imm.hideSoftInputFromWindow(getCurrentFocus().getWindowToken(), 0);
+        return true;
+    }
+    public void onClickAddDetails(View view) {
+        ContentValues values = new ContentValues();
+        values.put(UserProvider.name, ((EditText) findViewById(R.id.txtName)).getText().toString());
+        getContentResolver().insert(UserProvider.CONTENT_URI, values);
+        Toast.makeText(getBaseContext(), "New Record Inserted", Toast.LENGTH_LONG).show();
+    }
+
+    public void onClickShowDetails(View view) {
+        // Retrieve employee records
+        TextView resultView= (TextView) findViewById(R.id.res);
+        Cursor cursor = getContentResolver().query(Uri.parse("content://com.example.brunoorocha.petshop.UserProvider/users"), null, null, null, null);
+        if(cursor.moveToFirst()) {
+            StringBuilder strBuild=new StringBuilder();
+            while (!cursor.isAfterLast()) {
+                strBuild.append("\n"+cursor.getString(cursor.getColumnIndex("id"))+ "-"+ cursor.getString(cursor.getColumnIndex("name")));
+                cursor.moveToNext();
+            }
+            resultView.setText(strBuild);
+        }
+        else {
+            resultView.setText("No Records Found");
+        }
+    }
+}
